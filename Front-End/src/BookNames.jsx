38f0c4cd6e1e67d5630b70d6f1d7b2894568004b
Akash9698 <senthilsenthil98832@gmail.com>
2024-04-02 17:57:54 +0530

    front-end deployed

---
+++ Front-End/src/BookNames.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function BookNames() {
+  return (
+    <div>
+        <div id='firstarea'>
+            <h1>Explore Funiest Book Titles</h1>
+        </div>
+    </div>
+  )
+}
+
+export default BookNames
